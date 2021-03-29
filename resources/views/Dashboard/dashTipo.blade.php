fdda448839656d1fdbc9fc37c63e7fb49a80a68c
Faztudo Machava <fasthymachava12@gmail.com>
2021-03-29 23:18:46 +0100

    Ultima atualização com ajustes no componente de tipo de viatura

--- resources/views/Dashboard/dashTipo.blade.php
+++ resources/views/Dashboard/dashTipo.blade.php
@@ -1,7 +1,7 @@
 @extends('master.dashboard')
 
 @section('css')
-    <link href="{{asset('css/dataTables.bootstrap5.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/dataTables.bootstrap5.min.css') }}" rel="stylesheet">
 @endsection
 
 @section('titulo')
@@ -20,17 +20,17 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                            <div class="mb-3 me-3">
-                                <label for="txtNome" class="form-label">Nome</label>
-                                <input type="text" class="form-control" name="nome">
-                            </div>
-                            <div class="mb-3">
-                                <label for="txtDescricao" class="form-label">Descrição</label>
-                                <textarea type="text" class="form-control" name="descricao"></textarea>
-                            </div>
+                        <div class="mb-3 me-3">
+                            <label for="txtNome" class="form-label">Nome</label>
+                            <input type="text" class="form-control" name="nome">
+                        </div>
+                        <div class="mb-3">
+                            <label for="txtDescricao" class="form-label">Descrição</label>
+                            <textarea type="text" class="form-control" name="descricao"></textarea>
+                        </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="button" class="btn" data-bs-dismiss="modal">Cancelar</button>
                         <button type="submit" class="btn btn-primary">Adicionar</button>
                     </div>
                 </div>
@@ -41,46 +41,50 @@
 
 
     <!-- ================================== Editar=============================== -->
-    <form id="formUpdTipo" method="POST" action="/editTipo">
-        <div class="modal fade" id="editar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="lblModelAdd">Atualizar Tipo de viatura</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
+
+    <div class="modal fade" id="edit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="lblModelAdd">Atualizar Tipo de viatura</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form id="formUpdTipo" method="POST" action="/editTipo">
+                    {{ csrf_field() }}
+                    {{ method_field('PUT') }}
                     <div class="modal-body">
                         <div class="mb-3 me-3">
-                                <label for="txtNome" class="form-label">Nome</label>
-                                <input type="text" id="txtNome" class="form-control" name="nome">
-                            </div>
-                            <div class="mb-3">
-                                <label for="txtDescricao" class="form-label">Descrição</label>
-                                <textarea type="text" id="txtDescricao" class="form-control" name="descricao"></textarea>
-                            </div>
+                            <label for="txtNome" class="form-label">Nome</label>
+                            <input type="text" id="txtNome" class="form-control" name="nome">
+                        </div>
+                        <div class="mb-3">
+                            <label for="txtDescricao" class="form-label">Descrição</label>
+                            <textarea type="text" id="txtDescricao" class="form-control" name="descricao"></textarea>
+                        </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn" data-bs-dismiss="modal">Cancelar</button>
                         <button type="submit" class="btn btn-primary">Atualizar</button>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
-    </form>
+    </div>
+
 
     <!-- =================================== Delete User =================================== -->
-    <form id="formDelTipo" action="/delTipo" method="POST">
-        <div class="modal fade" id="deletar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            {{ csrf_field() }}
-            {{ method_field('DELETE') }}
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="lblModelAdd">Eliminar cliente</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
+
+    <div class="modal fade" id="deletar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="lblModelAdd">Eliminar cliente</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form id="formDelTipo" action="/delTipo" method="POST">
+                    {{ csrf_field() }}
+                    {{ method_field('DELETE') }}
                     <div class="modal-body">
                         <input type="hidden" name="_method" value="DELETE">
                         <P>Tem certeza que deseja eliminar este tipo de viatura?</P>
@@ -89,10 +93,11 @@
                         <button type="button" class="btn" data-bs-dismiss="modal">Cancelar</button>
                         <button type="submit" class="btn btn-primary">Confirmar</button>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
-    </form>
+    </div>
+
     <!-- ================================== Delete User ==================================== -->
 
 
@@ -105,17 +110,18 @@
     <div class="container-fluid">
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Tipos de viatura</h1>
-            <button href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" data-bs-toggle="modal" data-bs-target="#adicionar"><i class="bi bi-person-plus"></i> Novo tipo de viatura</button>
+            <button href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" data-bs-toggle="modal"
+                data-bs-target="#adicionar"><i class="bi bi-person-plus"></i> Novo tipo de viatura</button>
         </div>
         <div class="row">
             <div class="col-xl-3 col-md-6 mb-4">
                 <div class="card border-left-primary shadow h-100 py-2">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
+                            <div class="text-center col mr-2">
                                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                     Numero total de tipos de viaturas</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{$totalTipos}}</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalTipos }}</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -138,7 +144,7 @@
                                 <th>Nome</th>
                                 <th>Descrição</th>
                                 <th>Data de registo</th>
-                                <th colspan="2">Acções</th>
+                                <th>Acções</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -149,11 +155,10 @@
                                     <td>{{ $tipos->descrição }}</td>
                                     <td>{{ $tipos->data }}</td>
                                     <td>
-                                        <button id="btnEditar" class="btn btn-primary btnEditar"
-                                            data-bs-toggle="modal" data-bs-target="#editar"><i  class="bi bi-pencil-square"></i></button>
-                                    </td>
-                                    <td>
-                                        <a href="/delTipo/{{$tipos->id}}" class="btn btn-danger btnEliminar" ><i class="bi bi-trash"></i></a>
+                                        <a href="#" id="btnEditar" class="btn btn-primary btnEditar"><i
+                                                class="bi bi-pencil-square"></i></a>
+                                        <a href="#" class="btn btn-danger btnEliminar"><i
+                                                class="bi bi-trash"></i></a>
                                     </td>
                                 </tr>
                             @endforeach
@@ -166,5 +171,5 @@
 @endsection
 
 @push('javascript')
-<script src="{{asset('js/Tipo.js')}}"></script>
+    <script src="{{ asset('js/TipoViatura.js') }}"></script>
 @endpush
